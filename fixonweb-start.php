f8530dd9b0b3e5abf4d79e32f8fe9bd91f36a73c
edinaldohvieira <edinaldohvieira@gmail.com>
2020-04-07 18:38:41 -0300

    0.1.0 - ajustando definições iniciais

---
+++ fixonweb-start.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     fixonweb-start
+ * Plugin URI:      https://github.com/edinaldohvieira/fixonweb-start
+ * Description:     Redireciona conforme a função a que o usuário pertence.
+ * Author:          Edinaldo H Vieira
+ * Author URI:      https://github.com/edinaldohvieira
+ * Text Domain:     fixonweb-start
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fixonweb_Start
+ */
+
+/*
+fix158629
+*/
