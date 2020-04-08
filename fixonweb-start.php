d81e677ee518a142c46bf71744c07b2ae56c7791
edinaldohvieira <edinaldohvieira@gmail.com>
2020-04-08 10:31:15 -0300

    0.1.6.1 - Meios de atualização automática

--- fixonweb-start.php
+++ fixonweb-start.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/edinaldohvieira
  * Text Domain:     fixonweb-start
  * Domain Path:     /languages
- * Version:         0.1.2
+ * Version:         0.1.6
  *
  * @package         Fixonweb_Start
  */
