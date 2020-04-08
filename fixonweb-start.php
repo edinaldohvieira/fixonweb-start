d9249c315dc9f246d335812c0c551cc26c7fd8da
edinaldohvieira <edinaldohvieira@gmail.com>
2020-04-08 12:00:49 -0300

    0.1.10 - primeiro codigo parse_request

--- fixonweb-start.php
+++ fixonweb-start.php
@@ -7,15 +7,20 @@
  * Author URI:      https://github.com/edinaldohvieira
  * Text Domain:     fixonweb-start
  * Domain Path:     /languages
- * Version:         0.1.9
+ * Version:         0.1.10
  *
  * @package         Fixonweb_Start
  */
 
 /*
+Código de identificação deste plugin
 fix158629
 */
 
+
+/*
+ATUALIZAÇÃO DESTE PLUGIN VIA GITHUB
+*/
 require 'plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/edinaldohvieira/fixonweb-start',
@@ -23,11 +28,17 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'fixonweb-start/fixonweb-start'
 );
 
+
+/*
+INTERCEPTAR A URL E AGIR DE ACORDO COM AS PERMISSÕES
+*/
+
 add_action( 'parse_request', 'wmc_parse_request');
 function wmc_parse_request( &$wp ) {
 	if($wp->request == 'start'){
 		if(current_user_can('administrativo')){
 			wp_redirect( home_url().'/administrativo/' );
+			exit;
 		}
 		wp_redirect( home_url() );
 		exit;
